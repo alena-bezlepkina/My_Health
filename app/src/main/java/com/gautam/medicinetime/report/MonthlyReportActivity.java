fbb6be760b11728190a8313e1b8fd99b482cfa91
alena-bezlepkina <alenabezlepkina@mail.ru>
2021-12-01 18:15:13 +0100

    Init_5

--- app/src/main/java/com/gautam/medicinetime/report/MonthlyReportActivity.java
+++ app/src/main/java/com/gautam/medicinetime/report/MonthlyReportActivity.java
@@ -50,7 +50,7 @@ public class MonthlyReportActivity extends AppCompatActivity {
             presenter.setFiltering(taskFilterType);
         }
     }
-
+// item
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home){
